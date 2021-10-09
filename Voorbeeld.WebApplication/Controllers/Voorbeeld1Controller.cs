93b9a7edbbfd0b71a158bd447cf99ff4e92a860e
rolandwardenaar <roland.wardenaar@gmail.com>
2021-10-09 22:37:15 +0200

    shoppingcart

--- Voorbeeld.WebApplication/Controllers/Voorbeeld1Controller.cs
+++ Voorbeeld.WebApplication/Controllers/Voorbeeld1Controller.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Voorbeeld.WebApplication.Models;
 
 namespace Voorbeeld.WebApplication.Controllers
 {
@@ -10,15 +13,35 @@ namespace Voorbeeld.WebApplication.Controllers
         private readonly ILogger<Voorbeeld1Controller> _logger;
         private readonly IConfiguration _configuration;
 
+        public static ShoppingCart ShoppingCart { get; set; }
+
         public Voorbeeld1Controller(ILogger<Voorbeeld1Controller> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
+            if (ShoppingCart == null) ShoppingCart = new ShoppingCart();
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(ShoppingCart);
+        }
+
+        public IActionResult ClearCart()
+        {
+            ShoppingCart.Articles.Clear();
+            return View("Index",ShoppingCart);
+        }
+        public List<string> AddArticle(string name)
+        {
+            ShoppingCart.Articles.Add(name);
+
+            foreach (var item in ShoppingCart.Articles)
+            {
+                Debug.WriteLine(item);
+            }
+
+            return ShoppingCart.Articles;
         }
 
         [HttpGet("/api/getstripjson/{id}")]
---
+++ Voorbeeld.WebApplication/Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Voorbeeld.WebApplication.Models
+{
+    public class ShoppingCart
+    {
+        public List<string> Articles { get; set; }
+
+        public ShoppingCart()
+        {
+            Articles = new List<string>();
+        }
+    }
+}
